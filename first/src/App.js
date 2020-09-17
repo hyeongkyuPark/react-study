42eb381716bfefc7265f3243ce94a7394cec1eb3
parkhyeongkyu <phg0644@gmail.com>
2020-09-17 10:46:25 +0900

    useState를 활용한 Counter구현

--- first/src/App.js
+++ first/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import Counter from './Counter';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello isSpecial={true}/>
-      <Hello name='react' />
-    </Wrapper>
+    <>
+      <Wrapper name='props, 조건부렌더링'>
+        <Hello isSpecial={true}/>
+        <Hello name='react' />
+      </Wrapper>
+      <Wrapper name='Counter(useState)'>
+        <Counter />
+      </Wrapper>
+    </>
   );
 }
 
--- first/src/Wrapper.js
+++ first/src/Wrapper.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-function Wrapper({ children }) {
+function Wrapper({ children, name }) {
     const styel = {
         border: '2px solid black',
-        padding: 16
+        padding: '0 16px 20px 16px',
+        marginBottom: 20
     }
     
     return (
-    <div style={styel}>{children}</div>
+        <div style={styel}>
+            <h1>{name}</h1>
+            <div>{children}</div>
+        </div>
     )
 }
 
