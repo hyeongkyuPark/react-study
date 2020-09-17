b0d4e5885e68622a98df6cb7c10559bc8a6de7ab
parkhyeongkyu <phg0644@gmail.com>
2020-09-17 11:49:00 +0900

    useState를 활용한 Input 상태 객체로 관리

--- first/src/InputSample.js
+++ first/src/InputSample.js
@@ -2,21 +2,48 @@ import React, { useState } from 'react';
 
 function InputSample() {
 
-    const [text, setText] = useState('');
-    
+    const [inputs, setInputs] = useState({
+       name: '',
+       nickname: '' 
+    });
+
+    const {name, nickname} = inputs;
+
     const onChange = (e) => {
-        setText(e.target.value);
+        const { name, value } = e.target;
+
+        const nextInputs = {
+            ...inputs,
+            [name]: value
+        };
+
+        setInputs(nextInputs);
     }
-    const onReset = (e) => {
-        setText('');
+    
+    const onReset = () => {
+        setInputs({
+            name: '',
+            nickname: '' 
+         });
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input 
+                name='name' 
+                placeholder="이름" 
+                onChange={onChange}
+                value={name}
+            />
+            <input 
+                name='nickname' 
+                placeholder="닉네임" 
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+    <b>값: {name}({nickname})</b>
             </div>
         </div>
     )
